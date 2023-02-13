090a1b811172a662e7aecf7d0f92db10da244448
WilliamNordqvist <william.nordqvist@crowdcollective.com>
2023-02-13 15:15:03 +0100

    created Sections

---
+++ components/section/section.tsx
@@ -0,0 +1,7 @@
+export const Section = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className=" text-white h-screen flex items-center justify-center">
+            {children}
+        </div>
+    )
+}
