d8fe35b6957955d50c54d6e7485af82b64ff2cb5
WilliamNordqvist <william.nordqvist@crowdcollective.com>
2023-02-20 13:02:02 +0100

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Background } from "@/components/background/background";
 import "./globals.css";
 import { Exo } from "@next/font/google";
+import { AnalyticsWrapper } from "@/components/analytics/analytics";
 
 const ubuntu = Exo({
   weight: ["100", "200", "300", "400", "500"],
@@ -14,7 +15,10 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={ubuntu.className}>
-        <Background>{children}</Background>
+        <Background>
+          {children}
+          <AnalyticsWrapper/>
+          </Background>
       </body>
     </html>
   );
