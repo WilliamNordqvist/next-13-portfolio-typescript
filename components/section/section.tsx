1439f82254a6dcf1f1ad2da3a631b51524c27358
WilliamNordqvist <william.nordqvist@crowdcollective.com>
2023-02-20 16:08:02 +0100

    minor bugg fixes

--- components/section/section.tsx
+++ components/section/section.tsx
@@ -1,7 +1,8 @@
 export const Section = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className=" p-5 sm:p-0 text-white min-h-screen flex items-center justify-center font-thin ">
+    <div className="max-w-screen-2xl m-auto p-5 sm:p-0 text-white min-h-screen flex items-center justify-center font-thin ">
       {children}
     </div>
   );
 };
+
