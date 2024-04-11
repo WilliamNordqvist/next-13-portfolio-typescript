c57c8d8fcec1d2c772434cad1e634fd4c626e556
William Nordqvist <wnordqvist@gmail.com>
2024-04-11 15:41:08 +0200

    update cv

--- components/cv/cv.tsx
+++ components/cv/cv.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import { CvIcon } from "../icons/cv";
+import Link from "next/link";
 import { Title } from "../title/title";
 
 export const CV = () => {
@@ -7,7 +7,7 @@ export const CV = () => {
     <div className="flex flex-col items-center">
       <Title>My CV</Title>
       <Link
-        href="https://assets.ctfassets.net/dgbqz1h8siux/1STy1sjXfeRf5aCo9krQVv/eb6f56af531d508befec9a9a441d59e9/WilliamFE2023.pdf"
+        href="https://assets.ctfassets.net/dgbqz1h8siux/1STy1sjXfeRf5aCo9krQVv/2abbe651f461a51a042301c3383747e1/WilliamN_FE_2024.pdf"
         download="WilliamNordvist - CV/Personal Letter"
       >
         <CvIcon />
