ac7cfb45074bcd11f85df5e7b9e5ee7681243fdf
William Nordqvist <wnordqvist@gmail.com>
2024-04-12 15:00:04 +0200

    update tsparticles

--- components/background/background.tsx
+++ components/background/background.tsx
@@ -1,25 +1,27 @@
 "use client";
-import { useCallback } from "react";
-import type { Container, Engine } from "tsparticles-engine";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useState } from "react";
+
+import { loadSlim } from "@tsparticles/slim";
 
 export const Background = ({ children }: { children: React.ReactNode }) => {
-  const particlesInit = useCallback(async (engine: Engine) => {
-    await loadFull(engine);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  const particlesLoaded = useCallback(
-    async (container: Container | undefined) => {},
-    []
-  );
+  if (!init) return <></>;
 
   return (
     <>
       <Particles
         id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
         options={{
           fpsLimit: 250,
           particles: {
@@ -27,39 +29,29 @@ export const Background = ({ children }: { children: React.ReactNode }) => {
               value: "#ffffff",
             },
             move: {
-              enable: true,
-              // speed: $isMobile ? 0.3 : 0.5,
-              speed: 0.5,
               direction: "topLeft",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
               random: true,
+              speed: 0.5,
               straight: false,
             },
             number: {
-              // value: $isMobile ? 100 : 250,
-              value: 400,
               density: {
                 enable: true,
-                value_area: 1710.2328774690454,
               },
+              value: 400,
             },
             opacity: {
               value: 0.8,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 0.5,
-                opacity_min: 0,
-                sync: false,
-              },
             },
-
             shape: {
               type: "circle",
             },
             size: {
-              // value: $isMobile ? 1 : 1.5,
               value: { min: 1, max: 1.5 },
-              random: true,
             },
           },
           detectRetina: true,
